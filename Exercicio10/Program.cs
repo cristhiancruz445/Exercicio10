202a55e321b1db30103ab12822e0d7bc169a5990
Cristhian Rafael da Cruz <cristhiancruz445@gmail.com>
2026-01-31 14:10:25 -0300

    Initial commit

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio10
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] horas = Console.ReadLine().Split(' ');
+            int horaInicio = int.Parse(horas[0]);
+            int horaFim = int.Parse(horas[1]);
+
+            if(horaInicio > horaFim)
+            {
+                horaFim += 24;
+                int duracao = horaFim - horaInicio;
+                Console.WriteLine($"O JOGO DUROU {duracao} HORAS");
+            }
+            else if(horaInicio == horaFim)
+            {
+                Console.WriteLine("O JOGO DUROU 24 HORAS");
+            }
+            else
+            {
+                int duracao = horaFim - horaInicio;
+                Console.WriteLine($"O JOGO DUROU {duracao} HORAS");
+            }
+        }
+    }
+}
